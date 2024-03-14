f1001dba04aa0a85ea3aa42cf39021e1a49ee716
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-03-14 00:02:29 -0300

    chore: renomeia arquivo

--- libs/chess_crawler.py
+++ libs/chess_crawler.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 import json
 
-def getPlayer(username):
+def get_player(username):
     url = f"https://api.chess.com/pub/player/{username}"
     response = urlopen(url)
     data = json.load(response)
