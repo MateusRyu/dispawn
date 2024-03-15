30385b8674db93c124d27420c68e8b9f7bed043e
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-03-15 00:08:39 -0300

    feat: adiciona tratamento de exceção caso retorne código de erro 404

--- libs/chess_crawler.py
+++ libs/chess_crawler.py
@@ -1,8 +1,24 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 import json
 
 def get_player(username):
     url = f"https://api.chess.com/pub/player/{username}"
+
+    try:
+        response = urlopen(url)
+        data = json.load(response)
+    except HTTPError as error:
+        if error.code == 404:
+            print(f"Error 404: username '{username}' not found.")
+        else:
+            prin(f"Error {error.code}: search failed.")
+        return False
+    except Exception as error:
+        print(f"Error: {error}.")
+        return False
+
+    return data
     response = urlopen(url)
     data = json.load(response)
     return data
