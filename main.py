aeba3c24463b6e15c6b9dacef8b9a8b5375674d0
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-03-21 22:09:08 -0300

    feat: Adiciona modulo de banco de dados no bot

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from dotenv import load_dotenv
 import os
 import discord
+from libs.Database import Database
 
 load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
+MONGODB_TOKEN = os.getenv('MONGODB_TOKEN')
 
+db = Database(MONGODB_TOKEN)
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -13,6 +16,7 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    print(f'Living at {len(client.guilds)} guild(s)...')
 
 
 @client.event
@@ -22,5 +26,9 @@ async def on_message(message):
 
     if message.content.startswith(';hello'):
         await message.channel.send('Hello!')
+
+    elif message.content.startswith(';db_ping'):
+        feedback = 'Banco de dados conectado' if db.ping() else 'Banco de dados não conectado'
+        await message.channel.send(feedback)
 
 client.run(TOKEN)
