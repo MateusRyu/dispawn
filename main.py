e02a84592a51891a2a95773c63b0fd9770505e96
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-03-25 02:10:00 -0300

    feat: Converte cliente bot para um bot de comandos

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import os
 import discord
+from discord import Intents
+from discord.ext import commands
 from libs.Database import Database
 
 load_dotenv()
@@ -8,27 +10,22 @@ TOKEN = os.getenv('BOT_TOKEN')
 MONGODB_TOKEN = os.getenv('MONGODB_TOKEN')
 
 db = Database(MONGODB_TOKEN)
-intents = discord.Intents.default()
+intents = Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix=".", intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
-    print(f'Living at {len(client.guilds)} guild(s)...')
+    print(f'We have logged in as {bot.user}')
+    print(f'Living at {len(bot.guilds)} guild(s)...')
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+@bot.command()
+async def ping(ctx):
+    print("pong")
+    if ctx.author == bot.user:
         return
+    await ctx.reply(f'Pong! In {round(bot.latency * 1000)}ms')
 
-    if message.content.startswith(';hello'):
-        await message.channel.send('Hello!')
-
-    elif message.content.startswith(';db_ping'):
-        feedback = 'Banco de dados conectado' if db.ping() else 'Banco de dados não conectado'
-        await message.channel.send(feedback)
-
-client.run(TOKEN)
+bot.run(TOKEN)
