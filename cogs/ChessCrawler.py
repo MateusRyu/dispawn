054ec6d5d1affe51768176fca2d6ff9859d81b64
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-03-25 17:48:38 -0300

    feat: Cria condicional para o caso do username não existir

--- cogs/ChessCrawler.py
+++ cogs/ChessCrawler.py
@@ -23,6 +23,12 @@ class ChessCrawler(commands.Cog):
         print("Start to get profile..")
         username = user or ctx.author
         profile = get_player(username)
+        
+        if profile == False:
+            await ctx.reply(f'Jogador "{username}" não encontrado...')
+            return False
+
+
         now = datetime.now()
         timestamp = now.strftime(self.timeformat)
 
@@ -47,18 +53,18 @@ class ChessCrawler(commands.Cog):
         embed.add_field(name="Streamer", value=self.convert_boolean(profile["is_streamer"]))
         if "twitch_url" in profile:
             embed.add_field(name="Twitch", value=profile["twitch_url"])
-        embed.add_field(name="Verificado", value=self.convert_boolean(profile["verified"]))
         embed.add_field(name="Liga", value=profile["league"])
 
         if "title" in profile:
             embed.add_field(name="Titulo", value=profile["title"])
+
+        if "fide" in profile:
             embed.add_field(name="Rating FIDE", value=profile["fide"])
 
 
         embed.set_footer(text=f"Dados coletados em: {timestamp}")
         embed.color = discord.Color.green()
 
-        await ctx.send(str(profile))
         await ctx.send(embed=embed)
         print("Profile was returned to the author!")
 
