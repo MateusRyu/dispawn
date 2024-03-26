ee51fb011ff86774e1bb31f3b4aaf5ba3da0b42c
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-03-26 02:25:09 -0300

    feat: cria função para requisitar stats do jogador na API do chess.com

--- libs/chess_crawler.py
+++ libs/chess_crawler.py
@@ -34,4 +34,20 @@ def get_titled_player_usernames_by_title(title_abbreviation):
     return data["players"]
 
 def get_player_stats(username):
+    url = f"https://api.chess.com/pub/player/{username}/stats"
+
+    try:
+        response = urlopen(url)
+        data = json.load(response)
+    except HTTPError as error:
+        if error.code == 404:
+            print(f"Error 404: username '{username}' not found.")
+        else:
+            prin(f"Error {error.code}: search failed.")
+        return False
+    except Exception as error:
+        print(f"Error: {error}.")
+        return False
+
+    return data
 
